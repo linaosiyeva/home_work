5e06d6524179309526c7dc3ebb7835fb82dc7b0c
linaosiyeva <62384783+linaosiyeva@users.noreply.github.com>
2020-04-11 23:07:54 +0300

    Update and rename 3_1.txt to 3_1.py

--- 3_1.py
+++ 3_1.py
@@ -1,3 +1,5 @@
+#Записати в стрічку філософію Пайтона 
+
 Zen="""Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -17,9 +19,15 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
+
+#Вивести весь текст у верхньому регістрі (всі великі літери)
 print(f'Uppercase text {Zen.upper()}')
+
+#Замінити всі входження символу “і” на “&”
 e=Zen.replace('i','&')
 print(f'Exchange i to & {e}')
+
+#Знайти в заданій стрічці кількість входжень слів (better, never, is)
 b=Zen.count('better')
 print(f'Number of the word better is {b}')
 n=Zen.count('never')
