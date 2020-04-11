a439b82ebe06b57a85c211959a713da359c191f7
linaosiyeva <62384783+linaosiyeva@users.noreply.github.com>
2020-04-11 22:44:02 +0300

    Create Count_of_positives_sum_of_negatives.py

---
+++ Count_of_positives_sum_of_negatives.py
@@ -0,0 +1,23 @@
+#Count of positives / sum of negatives
+
+#Given an array of integers.
+
+#Return an array, where the first element 
+#is the count of positives numbers 
+#and the second element is sum of negative numbers.
+
+#If the input array is empty or null, return an empty array.
+#Example:
+#For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], 
+#you should return [10, -65].
+
+def count_positives_sum_negatives(arr):
+    if not arr:
+        return []
+    positive_num, negative_num = 0, 0
+    for num in arr:
+        if num > 0:
+            positive_num += 1
+        elif num < 0:
+            negative_num += num
+    return [positive_num, negative_num]
