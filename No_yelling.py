ac486d1e6f070cdcf4cf5885b5d9524e43ef4abc
linaosiyeva <62384783+linaosiyeva@users.noreply.github.com>
2020-04-11 22:36:35 +0300

    Create No_yelling.py

---
+++ No_yelling.py
@@ -0,0 +1,20 @@
+#No yelling!
+
+#Write a function taking in a string like 
+WOW this is REALLY amazing and returning Wow this is really amazing. 
+String should be capitalized and properly spaced. 
+Using re and string is not allowed.
+
+Examples:
+filter_words('HELLO CAN YOU HEAR ME') #=> Hello can you hear me
+filter_words('now THIS is REALLY interesting') #=> Now this is really interesting
+filter_words('THAT was EXTRAORDINARY!') #=> That was extraordinary!
+
+def filter_words(st):
+    return ' '.join(st.capitalize().split())
+
+def filter_words(st):
+    st = st.lower()
+    new = st[0]
+    st = st[1:]
+    return ' '.join(str(new.upper() + st).split())
